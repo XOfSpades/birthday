2ce4398aa0691774283ed8f27ed2ee949bbdbae3
bstoecker <bernhard.stoecker@invision.de>
2016-07-23 22:47:40 +0200

    Squash routes into resouces

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  post 'person/create', to: 'person#create'
-  put 'person/:id', to: 'person#update'
-  delete 'person/:id', to: 'person#destroy'
-  get 'person/:id', to: 'person#show'
-  get 'person', to: 'person#index'
-  get 'person/new', to: 'person#new'
+  resources :person
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
