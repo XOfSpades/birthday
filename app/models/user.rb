e150a49b8bc351aa8e243a99973c13fdf96ae45a
bstoecker <bernhard.stoecker@invision.de>
2016-07-25 00:00:43 +0200

    get guest list where current user is on list

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email
   has_many :parties, class_name: '::Party'
   has_many :wish_list_items, class_name: '::WishListItem'
+
+  def on_guest_list
+    Guest.where(user_id: id)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,4 +33,49 @@ describe User, type: :model do
       ).to eq false
     end
   end
+
+  describe '#on_guest_list' do
+    let(:user) { User.create(valid_attributes) }
+    let(:inviter1) do
+      User.create(
+        password: 'foobarbaz',
+        first_name: 'Ingo',
+        last_name: 'Igel',
+        email: 'ingo@igel.de',
+        gravatar: 'www.gravatar.com/ingo_igel'
+      )
+    end
+    let(:inviter2) do
+      User.create(
+        password: 'foobarbaz',
+        first_name: 'Kira',
+        last_name: 'Katze',
+        email: 'kira@katze.de',
+        gravatar: 'www.gravatar.com/kira_katze'
+      )
+    end
+    let(:party_one) do
+      Party.create(
+        date: Date.new(2016, 8, 6),
+        title: 'The big birthday party',
+        user: inviter1
+      )
+    end
+    let(:party_two) do
+      Party.create(
+        date: Date.new(2016, 9, 27),
+        title: 'The other big birthday party',
+        user: inviter2
+      )
+    end
+
+    it 'returns a list of parties you have signed to' do
+      guest1 = Guest.create(user: user, party: party_one)
+      guest2 = Guest.create(user: user, party: party_two)
+      guest_list = user.on_guest_list
+      expect(guest_list.size).to eq 2
+      expect(guest_list).to include(guest1)
+      expect(guest_list).to include(guest2)
+    end
+  end
 end
