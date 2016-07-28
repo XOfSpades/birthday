e28b35444e2e5ccf8339cfe39a2883335a436a8d
bstoecker <bernhard.stoecker@invision.de>
2016-07-28 21:43:44 +0200

    Fix destroy Reservation

--- app/controllers/wish_list_item_controller.rb
+++ app/controllers/wish_list_item_controller.rb
@@ -3,7 +3,12 @@ class WishListItemController < ApplicationController
 
   def reserve
     wish_list_item.update(attributes)
-    Reservation.create(user: current_user, wish_list_item: wish_list_item)
+    if attributes[:reserved]
+      Reservation.create(user: current_user, wish_list_item: wish_list_item)
+    else
+      reservation = Reservation.find_by(wish_list_item_id: wish_list_item.id)
+      reservation.destroy
+    end
     flash.notice = "#{wish_list_item.name} erfolgreich reserviert."
     redirect_to(controller: :party, action: :show, id: params[:party_id])
   end
@@ -15,9 +20,10 @@ class WishListItemController < ApplicationController
   end
 
   def attributes
+    return @attributes if @attributes
     parameters = params.require(:wish_list_item).permit(:reserved).to_h
 
     parameters[:reserved] = ('true' == parameters[:reserved])
-    parameters
+    @attributes ||= parameters
   end
 end
